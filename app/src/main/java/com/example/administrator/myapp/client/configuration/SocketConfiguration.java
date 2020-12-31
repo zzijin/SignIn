bd232ee929f6f3516b1e4dc8615f36f3128cc066
zzijin <zzijin0@163.com>
2020-12-31 19:11:35 +0800

    添加必要权限

--- app/src/main/java/com/example/administrator/myapp/client/configuration/SocketConfiguration.java
+++ app/src/main/java/com/example/administrator/myapp/client/configuration/SocketConfiguration.java
@@ -4,7 +4,7 @@ public class SocketConfiguration {
     /**
      *  服务器IP地址
      */
-    public static final String IP="192.168.43.237";
+    public static final String IP="100.70.108.192";
     /**
      * 测试用服务器IP地址
      */
