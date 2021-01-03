bdd60b0910abe896fa39e2dedfcfbe69801e038c
zzijin <zzijin0@163.com>
2021-01-03 23:00:46 +0800

    增加bug

--- app/src/main/java/com/example/administrator/myapp/adapter/ActivityListAdapter.java
+++ app/src/main/java/com/example/administrator/myapp/adapter/ActivityListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.administrator.myapp.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 
 import com.example.administrator.myapp.Info.CheckInActivityInfo;
 import com.example.administrator.myapp.R;
+import com.example.administrator.myapp.baidumap.sign;
+import com.example.administrator.myapp.configuration.MessageNameConfiguration;
 
 import java.util.ArrayList;
 
@@ -29,7 +32,7 @@ public class ActivityListAdapter extends ArrayAdapter<CheckInActivityInfo> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View view  = LayoutInflater.from(getContext()).inflate(resourceId, null);
-        CheckInActivityInfo checkInActivityInfo=getItem(position);
+        final CheckInActivityInfo checkInActivityInfo=getItem(position);
         ImageView activityCover=view.findViewById(R.id.item_activity_cover);
         TextView activityTheme=view.findViewById(R.id.item_activity_theme);
         TextView activityInitiator=view.findViewById(R.id.item_activity_initiator);
@@ -42,6 +45,14 @@ public class ActivityListAdapter extends ArrayAdapter<CheckInActivityInfo> {
                 activitySignInStatus.setText("签到状态:"+checkInActivityInfo.getActivityParticipant().get(i).clockIn);
             }
         }
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(), sign.class);
+                intent.putExtra(MessageNameConfiguration.ACTIVITY_ID,checkInActivityInfo.getActivityID());
+                getContext().startActivity(intent);
+            }
+        });
         return view;
     }
 }
